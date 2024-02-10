24c4837a49ccd713c6c31e9a4dc82df8e09ea2c7
falak <falakgala09@gmail.com>
2024-02-10 14:15:30 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import CodeEditor from '@/components/CodeEditor';
+export default function Home() {
+  return (
+    <main>
+      <div className="code-editor-ref">
+        <CodeEditor />
+      </div>
+    </main>
+  );
+}
